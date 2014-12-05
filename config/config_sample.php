e8d17be56089fbf11177850281ea680f367c2a4e
AMWN <amwn85@gmail.com>
2014-12-05 13:11:17 +0100

    configsample

---
+++ config/config_sample.php
@@ -0,0 +1,31 @@
+ */<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+    'general' => array(
+        'aol' => 1
+    ),
+    'db' => array(
+        'username' => '',
+        'password' => '',
+    ),
+    'profitlocal' => array(
+        'username' => 'demo',
+        'password' => '',
+        'environmentid' => 'EA_10_24',
+        'url' => 'https://192.168.1.230/profitservices/'
+    ),
+    'profitaol' => array(
+        'username' => '',
+        'password' => '',
+        'environmentid' => '',
+        'url' => 'https://profitweb.afasonline.nl/profitservices/'
+    )
+);
+
+
